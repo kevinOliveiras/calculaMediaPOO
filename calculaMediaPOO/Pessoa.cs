2b5782eb176f74040df9de4e8d1a6132ee4e04d9
kevin <kevin@DESKTOP-1NVQ64V>
2023-10-02 03:23:35 -0300

    Adicionar arquivos de projeto.

---
+++ calculaMediaPOO/Pessoa.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculaMediaPOO
+{
+    internal class Pessoa
+    {
+        private int matricula;
+        private string nome;
+        private double notaProva1;
+        private double notaProva2;
+        private double notaTrabalho;
+
+        //Construtor que inicializa o nome e matricula do aluno
+        public Pessoa()
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+        }
+
+        //Propriedades que vão armazenar os valores informado pelo usuário
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+        public double NotaProva1
+        {
+            get { return notaProva1; }
+            set { notaProva1 = value; }
+        }
+        public double NotaProva2
+        {
+            get { return notaProva2; }
+            set { notaProva2 = value; }
+        }
+        public double NotaTrabalho
+        {
+            get { return notaTrabalho; }
+            set { notaTrabalho = value; }
+        }
+    }
+}
---
+++ calculaMediaPOO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculaMediaPOO
+{
+    internal class Program
+    {
+
+        //Método Main para o programa ser executável 
+        static void Main(string[] args)
+        {
+            Pessoa aluno;
+
+            //Criar um objeto "aluno" e pedir para o usuário introduzir seus dados
+            Console.WriteLine("Olá, vamos calcular sua média!!");
+            Console.WriteLine("Digite seu nome: ");
+            aluno = new Pessoa();
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua matricula: ");
+            aluno.Matricula = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da sua Primeira Prova: ");
+            aluno.NotaProva1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da sua Segunda Prova: ");
+            aluno.NotaProva2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do seu trabalho: ");
+            aluno.NotaTrabalho = Convert.ToDouble(Console.ReadLine());
+
+            double mediaProvas = (aluno.NotaProva1 + aluno.NotaProva2) * 2.5;
+            double mediaFinal = (aluno.NotaTrabalho * 2 + mediaProvas) / 7.5;
+
+            Console.WriteLine("Nome: " + aluno.Nome);
+            Console.WriteLine("Matricula: " + aluno.Matricula);
+            Console.WriteLine($"Media final: {mediaFinal}");
+
+            Console.ReadKey();
+        }
+    }
+}
